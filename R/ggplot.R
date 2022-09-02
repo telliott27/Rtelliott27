0b5db7f9b79e4926460bb93a68ec531bffdf44da
Thomas <telliott@fastmail.fm>
2022-09-02 11:56:29 -0700

    add theme_inky_blue

--- R/ggplot.R
+++ R/ggplot.R
@@ -8,3 +8,20 @@ theme_clean <- function(base_size = 11, base_family = "Arial") {
     )
   th
 }
+
+
+#' @export
+theme_inky_blue <- function(base_size = 11, base_family = "Arial") {
+  th <- theme_minimal(base_family = base_family, base_size = base_size) +
+    theme(
+      plot.background = element_rect(fill = ps_colors2$blue),
+      panel.background = element_rect(fill = ps_colors2$blue),
+      axis.text = element_text(color = "white"),
+      panel.grid = element_blank(),
+      plot.title = element_text(color = ps_colors2$pink,
+                                face = "bold"),
+      axis.line = element_line(color = "white"),
+      axis.title = element_text(color = ps_colors2$pink)
+    )
+  th
+}
