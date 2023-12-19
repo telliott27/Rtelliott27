004d179d8fd0b848c9f12b3d713d56e471cd54ba
Thomas <thomas.elliott@sanity.io>
2023-12-19 08:38:28 -0800

    make project and dataset parameters

--- R/connections.R
+++ R/connections.R
@@ -2,11 +2,12 @@
 sanity.bq <- R6::R6Class(
   public = list(
     conn = NULL,
-    initialize = function() {
+    initialize = function(project.name = Sys.getenv("BQ_PROJECT"),
+                          dataset.name = Sys.getenv("BQ_DATASET")) {
       self$conn = DBI::dbConnect(
         bigrquery::bigquery(),
-        project = Sys.getenv("BQ_PROJECT"),
-        dataset = Sys.getenv("BQ_DATASET")
+        project = project.name,
+        dataset = dataset.name
       )
     },
     query = function(q) {
